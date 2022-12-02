059c820ef0d5236136e89ab7d7bc0cbab48ee7c5
annaminasyann <117832832+annaminasyann@users.noreply.github.com>
2022-12-03 00:02:38 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const utils = require('./utils');
 
-let text = "  hello world";
+let text = "  hello world ";
 let mat1 = [ [1, 4, 1],
              [2, 1, 1],
              [2, 5, 1]]
@@ -9,19 +9,13 @@ let mat2 = [[3, 2, 1],
             [1, 2 ,4],
             [2, 4, 3]]
 
-let matrix1 = [[1, 4, 1],
-               [2, 1, 1],
-               [2, 5, 1,]]
-
-let matrix2 = [[3, 2, 1],
-               [1, 2 ,4],
-               [2, 4, 3]]
-
 let mat = [[2, 4, 6],
            [5, 2, 3],
            [4, 6, 1]]
 
 console.log(utils.matrixProduct(mat1, mat2));
-console.log(utils.sumofMatrices(matrix1, matrix2));
+console.log(utils.sumofMatrices(mat1, mat2));
 console.log(utils.sumofEachRow(mat));
 console.log(utils.countWords(text));
+console.log(utils.trim(text));
+
