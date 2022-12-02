39390f74ca3f4f6a12c1bcc04f937ab9941e4353
annaminasyann <117832832+annaminasyann@users.noreply.github.com>
2022-12-03 00:01:59 +0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ function matrixProduct(mat1, mat2) {
         result[i] = [];
         for (let j = 0; j < mat2[0].length; j++) {
             let res = 0
-            for (let k = 0; k < mat1[i].length; k++) {
+            for (let k = 0; k < mat1[0].length; k++) {
                 res += mat1[i][k] * mat2[k][j];
             }
             result[i][j] = res;
@@ -19,14 +19,13 @@ function sumofMatrices(matrix1, matrix2){
     let result2 = [];
     for(let  i = 0; i < matrix1.length; i++) {
         result2[i] = [];
-        for(let j = 0; j < matrix1.length; j++) {
-            for (let k = 0; k < matrix1[i].length; k++){
-                    result2[i][j] = matrix1[j][k] + matrix2[j][k];
+        for(let j = 0; j < matrix2[0].length; j++) {
+            result2[i][j] = matrix1[i][j] + matrix2[i][j];
             }
+            
         }
     return result2;
     } 
-}
 console.log(sumofMatrices);
 
 
@@ -34,8 +33,8 @@ function sumofEachRow(mat){
     let sumOfRows = [];
     for (let i = 0; i< mat.length; i++){
         sumOfRows[i] = [];
-        let sum = 0;
-        for (let j = 0; j < mat[i].length; j++){
+        let sum = 0
+        for (let j = 0; j < mat[0].length; j++){
             sum = sum + mat[i][j];
         }
         sumOfRows[i] = sum;
@@ -45,27 +44,36 @@ function sumofEachRow(mat){
 console.log(sumofEachRow)
 
 
-function countWords(text){
+function countWords(string){
     let count = 0;
     let word = false;
-    for (let i=0; i<text.length; i++) {
-        if (text[i]===" ") {
+    for (let i=0; i<string.length; i++) {
+        if (string[i]==" ") {
            if(word){
            count++; 
            word = false;
-           }
-    }
+           } }
     else{
-            word = true; 
-        }
-    }
-    if  (word)
-        count++; 
+            word = true; }}
+    if(word)
+         count++; 
     return count;
 }
 console.log(countWords);
 
 
+function trim(string){
+    let space = " ";
+    let i = 0;
+    if (string[i] === " "){
+        space = string.substr(1, string.length - 1);
+    }
+    else if(string[i + string.kength-1] === " "){
+        space = string.substr(1, string.length - 1);
+    return space;
+    }
+}
+console.log(trim);
 
 
 
@@ -74,4 +82,5 @@ matrixProduct,
 sumofMatrices,
 sumofEachRow,
 countWords,
+trim
 }
